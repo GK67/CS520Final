b7b21a415c48c5816b6c44dbe2616822a8b7a71c
hoyinho <hoyinho999@gmail.com>
2018-12-10 17:47:30 -0500

    changed

--- dbProgram/Path.java
+++ dbProgram/Path.java
@@ -219,7 +219,7 @@ public class Path{
 		ArrayList neighbors = allNeighbors(node);
 		System.out.println(neighbors);
 		for (int i = 0; i < neighbors.size(); i++){
-		    PriorityQueue newPath = new PriorityQueue(path);
+		    PriorityQueue<ArrayList> newPath = new PriorityQueue<ArrayList>(path);
 		    String neighbor = (String) (neighbors.get(i));
 		    neighbor = neighbor.replaceAll("\\s+","");
 		    if (! newPath.contains(neighbor)){
