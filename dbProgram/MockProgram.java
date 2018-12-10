cafdca0f0ca58c970836bb07ceec599a653171d8
Ruifeng Zhang <ruifeng.zhang1993@gmail.com>
2018-12-10 03:09:34 -0500

    add the database program

---
+++ dbProgram/MockProgram.java
@@ -0,0 +1,120 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class MockProgram {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage: java VideoStore CUSTOMER_ID CUSTOMER_PASSWORD");
+            System.exit(1);
+        }
+
+        //create mock data
+        ArrayList output=new ArrayList();
+        int i=10;
+        String id="42949710";
+        Double la=42.3787077;
+        Double lo=42.3787077;
+        Double el=42.3787077;
+        for(;i<40;i++){
+
+            ArrayList node=new ArrayList();
+            node.add(id+Integer.toString(i));
+            node.add(la);
+            la+=i;
+            node.add(lo);
+            lo-=i;
+            node.add(el);
+            el-=i;
+            output.add(node);
+        }
+        
+        ArrayList outputWayList=new ArrayList();
+        
+        int j=0;
+        String idW="429497109";
+        String sName="abc";
+        String bName="bbb";
+
+        for(;j<10;j++){
+            ArrayList outputWay=new ArrayList();
+            ArrayList node=new ArrayList();
+            for(int n=0;n<=j;n++){
+                node.add(((ArrayList)output.get(n)).get(0));
+            }
+
+            outputWay.add(idW+Integer.toString(j));
+            if(j==3 ||j==5)
+                outputWay.add(null);
+            else
+                outputWay.add(sName+Integer.toString(j));
+
+            outputWay.add(node);
+
+            if(j==5 || j==7)
+                outputWay.add(null);
+            else
+                outputWay.add(bName+Integer.toString(j));
+            outputWayList.add(outputWay);
+        }
+
+        //create mock data end
+
+        /* prepare the database connection stuff */
+        Query q = new Query();
+        q.openConnection();
+        q.prepareStatements();
+
+        //only insert the data in the begining, each time running, it will delete the original tables
+        //and create new one
+        q.transaction_insert_data(output, outputWayList);  
+        System.out.println("Finish insert data");
+
+        //search a node, before get value need do this search first
+        q.transaction_search_node("4294971015");
+
+        //get the element of the node
+        System.out.println("latitude\t"+q.getLatitude());
+        System.out.println("latitude\t"+q.getLongitude());
+        System.out.println("latitude\t"+q.getElevation());
+
+        //search a node, before get value need do this search first
+        q.transaction_search_node("4294971017");
+
+        //get the element of the node
+        System.out.println("latitude\t"+q.getLatitude());
+        System.out.println("latitude\t"+q.getLongitude());
+        System.out.println("latitude\t"+q.getElevation());
+
+
+
+        //search ways by a node id
+        ArrayList wayList = q.transaction_search_way("4294971015");
+
+        for(int l=0;l<wayList.size();l++){
+            System.out.println("waylist:\t"+wayList.get(l));
+        }
+
+        //get all the nodes
+        ArrayList nodeList = q.transaction_search_all_node();
+
+        for(int l=0;l<nodeList.size();l++){
+            System.out.println("all nodes list:\t"+nodeList.get(l));
+        }
+
+        // search nodes by a way id
+        ArrayList nodeListOfAWay = q.transaction_search_nodes_of_way("4294971098");
+        //get all the nodes of a way
+        for(int l=0;l<nodeListOfAWay.size();l++){
+            System.out.println("all nodes list of a way:\t"+nodeListOfAWay.get(l));
+        }
+
+
+        q.closeConnection();
+
+    }
+
+}
+
---
+++ dbProgram/Query.java
@@ -0,0 +1,307 @@
+import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
+import java.io.FileInputStream;
+
+/**
+ * Runs queries against a back-end database
+ */
+public class Query {
+    private static Properties configProps = new Properties();
+
+    private static String osmdbUrl;
+
+    private static String postgreSQLDriver;
+    private static String postgreSQLUser;
+    private static String postgreSQLPassword;
+
+    private static ArrayList oneNode = null;
+
+    // DB Connection
+    private Connection _osmdb;
+    // Canned queries
+
+    private String _create_node_table_sql = "CREATE TABLE node ("
+  						+"id CHAR(30) NOT NULL,"
+  						+"latitude FLOAT NOT NULL,"
+  						+"longitude FLOAT NOT NULL,"
+  						+"elevation FLOAT NOT NULL,"
+  						+"PRIMARY KEY (id))";
+    private PreparedStatement _create_node_table_statement;
+
+    private String _create_way_table_sql = "CREATE TABLE way ("
+  						+"id CHAR(30) NOT NULL,"
+  						+"streetName CHAR(30),"
+  						+"nid CHAR(30) NOT NULL,"
+  						+"buildingName CHAR(30),"
+  						+"FOREIGN KEY(nid) REFERENCES node)";
+    private PreparedStatement _create_way_table_statement;
+
+    private String _insert_node_sql = "INSERT INTO node VALUES (?, ?, ?, ?)";
+    private PreparedStatement _insert_node_statement;
+
+    private String _insert_way_sql = "INSERT INTO way VALUES (?, ?, ?, ?)";
+    private PreparedStatement _insert_way_statement;
+    
+
+    private String _delete_table_node_sql = "DROP TABLE IF EXISTS node";
+    private PreparedStatement _delete_table_node_statement;
+
+    private String _delete_table_way_sql = "DROP TABLE IF EXISTS way";
+    private PreparedStatement _delete_table_way_statement;
+    
+
+    private String _search_node_by_nid_sql = "SELECT * FROM node WHERE id = ?";
+    private PreparedStatement _search_node_by_nid_statement;
+
+    private String _search_way_by_nid_sql = "SELECT id FROM way WHERE nid = ?";
+    private PreparedStatement _search_way_by_nid_statement;
+
+    private String _search_all_nodeId_sql = "SELECT id FROM node";
+    private PreparedStatement _search_all_nodeId_statement;
+
+    private String _search_node_by_wid_sql = "SELECT nid FROM way WHERE id = ?";
+    private PreparedStatement _search_node_by_wid_statement;
+
+
+
+
+    private String _begin_transaction_read_write_sql = "BEGIN TRANSACTION READ WRITE";
+    private PreparedStatement _begin_transaction_read_write_statement;
+
+    private String _begin_transaction_read_only_sql = "BEGIN TRANSACTION READ ONLY";
+    private PreparedStatement _begin_transaction_read_only_statement;
+
+    private String _commit_transaction_sql = "COMMIT TRANSACTION";
+    private PreparedStatement _commit_transaction_statement;
+     
+
+    public Query() {
+    }
+
+    /**********************************************************/
+    /* Connections to postgres databases */
+
+    public void openConnection() throws Exception {
+        configProps.load(new FileInputStream("dbconn.config"));
+        
+        
+        osmdbUrl        = configProps.getProperty("osmdbUrl");
+        postgreSQLDriver   = configProps.getProperty("postgreSQLDriver");
+        postgreSQLUser     = configProps.getProperty("postgreSQLUser");
+        postgreSQLPassword = configProps.getProperty("postgreSQLPassword");
+
+
+        /* load jdbc drivers */
+        Class.forName(postgreSQLDriver).newInstance();
+
+        /* open connections to database: osmdb database */
+        _osmdb = DriverManager.getConnection(osmdbUrl, // database
+                postgreSQLUser, // user
+                postgreSQLPassword); // password
+    }
+
+    public void closeConnection() throws Exception {
+        _osmdb.close();
+    }
+
+
+    public void prepareStatements() throws Exception {
+
+
+    	_create_node_table_statement= _osmdb.prepareStatement(_create_node_table_sql);
+        _create_way_table_statement = _osmdb.prepareStatement(_create_way_table_sql);
+        _insert_node_statement = _osmdb.prepareStatement(_insert_node_sql);
+        _insert_way_statement = _osmdb.prepareStatement(_insert_way_sql);
+
+
+
+        _begin_transaction_read_write_statement = _osmdb.prepareStatement(_begin_transaction_read_write_sql);
+
+        _begin_transaction_read_only_statement = _osmdb.prepareStatement(_begin_transaction_read_only_sql);
+
+        _commit_transaction_statement = _osmdb.prepareStatement(_commit_transaction_sql);
+
+
+
+        _delete_table_node_statement = _osmdb.prepareStatement(_delete_table_node_sql);
+        _delete_table_way_statement = _osmdb.prepareStatement(_delete_table_way_sql);
+   
+        _search_node_by_nid_statement = _osmdb.prepareStatement(_search_node_by_nid_sql);
+
+		_search_way_by_nid_statement = _osmdb.prepareStatement(_search_way_by_nid_sql);
+
+		_search_all_nodeId_statement = _osmdb.prepareStatement(_search_all_nodeId_sql);
+
+		_search_node_by_wid_statement= _osmdb.prepareStatement(_search_node_by_wid_sql);
+    }
+
+
+    public void transaction_insert_data(ArrayList outputNode, ArrayList outputWay) throws Exception {
+        
+       _begin_transaction_read_write_statement.executeUpdate();
+       
+       _delete_table_way_statement.executeUpdate();
+       _delete_table_node_statement.executeUpdate();
+
+       _create_node_table_statement.executeUpdate();
+       _create_way_table_statement.executeUpdate();
+
+       for(int i=0;i<outputNode.size();i++){
+       		_insert_node_statement.clearParameters();
+       		_insert_node_statement.setString(1,((ArrayList)outputNode.get(i)).get(0).toString());
+       		_insert_node_statement.setDouble(2,(Double)((ArrayList)outputNode.get(i)).get(1));
+       		_insert_node_statement.setDouble(3,(Double)((ArrayList)outputNode.get(i)).get(2));
+       		_insert_node_statement.setDouble(4,(Double)((ArrayList)outputNode.get(i)).get(3));
+       		_insert_node_statement.executeUpdate();		
+       }
+
+       for(int i=0;i<outputWay.size();i++){
+
+       		ArrayList nodeId = (ArrayList)(((ArrayList)outputWay.get(i)).get(2));
+       		String idW=((ArrayList)outputWay.get(i)).get(0).toString();
+
+       		Object sName=((ArrayList)outputWay.get(i)).get(1);
+       		Object bName=((ArrayList)outputWay.get(i)).get(3);
+
+       		for(int j=0;j<nodeId.size();j++){
+       			_insert_way_statement.clearParameters();
+       			_insert_way_statement.setString(1,idW);
+       			if(sName==null)
+       				_insert_way_statement.setString(2,"");
+       			else
+       				_insert_way_statement.setString(2,sName.toString());
+
+       			_insert_way_statement.setString(3,(nodeId.get(j)).toString());
+
+       			if(bName==null)
+       				_insert_way_statement.setString(4,"");
+       			else
+       				_insert_way_statement.setString(4,bName.toString());
+       			_insert_way_statement.executeUpdate();
+       		}
+       				
+       }
+       _commit_transaction_statement.executeUpdate();
+        
+    }
+
+
+    public Double getLatitude() throws Exception {
+        
+        if(oneNode==null)
+        	return null;
+        else{
+        	return (Double)(oneNode.get(0));
+        }
+    }
+
+    public Double getLongitude() throws Exception {
+        
+        if(oneNode==null)
+        	return null;
+        else{
+        	return (Double)(oneNode.get(1));
+        }
+    }
+
+    public Double getElevation() throws Exception {
+        
+        if(oneNode==null)
+        	return null;
+        else{
+        	return (Double)(oneNode.get(2));
+        }
+    }
+
+    public void transaction_search_node(String nodeId) throws Exception {
+        
+        _begin_transaction_read_only_statement.executeUpdate();
+        
+        _search_node_by_nid_statement.clearParameters();
+        _search_node_by_nid_statement.setString(1,nodeId);
+
+
+        ResultSet nodeSet = _search_node_by_nid_statement.executeQuery();
+        oneNode = new ArrayList();
+        if(nodeSet.next()){
+        	Double la= nodeSet.getDouble(2);
+        	Double lo= nodeSet.getDouble(3);
+        	Double el= nodeSet.getDouble(4);
+
+        	oneNode.add(la);
+        	oneNode.add(lo);
+        	oneNode.add(el);
+        }
+        else{
+        	oneNode= null;
+        }
+
+        _commit_transaction_statement.executeUpdate();
+    }
+
+    public ArrayList transaction_search_way(String nodeId) throws Exception {
+        
+        _begin_transaction_read_only_statement.executeUpdate();
+        
+        _search_way_by_nid_statement.clearParameters();
+        _search_way_by_nid_statement.setString(1,nodeId);
+
+
+        ResultSet waySet = _search_way_by_nid_statement.executeQuery();
+        ArrayList wayList = new ArrayList();
+        
+        while(waySet.next()){
+        	wayList.add(waySet.getString("id"));
+        }
+
+        _commit_transaction_statement.executeUpdate();
+
+        return wayList;
+    }
+
+    public ArrayList transaction_search_all_node() throws Exception {
+        
+        _begin_transaction_read_only_statement.executeUpdate();
+        
+        _search_all_nodeId_statement.clearParameters();
+
+        ResultSet nodeSet = _search_all_nodeId_statement.executeQuery();
+        ArrayList nodeList = new ArrayList();
+        
+        while(nodeSet.next()){
+        	nodeList.add(nodeSet.getString("id"));
+        }
+
+        _commit_transaction_statement.executeUpdate();
+
+        return nodeList;
+    }
+
+    public ArrayList transaction_search_nodes_of_way(String wayId) throws Exception {
+        
+        _begin_transaction_read_only_statement.executeUpdate();
+        
+        _search_node_by_wid_statement.clearParameters();
+        _search_node_by_wid_statement.setString(1,wayId);
+
+
+        ResultSet nodeSet = _search_node_by_wid_statement.executeQuery();
+        ArrayList nodeList = new ArrayList();
+        
+        while(nodeSet.next()){
+        	nodeList.add(nodeSet.getString("nid"));
+        }
+
+        _commit_transaction_statement.executeUpdate();
+
+        return nodeList;
+    }
+  
+}
